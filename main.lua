9e98f38529680fddf70ef892cbfa02227e7b00ac
jjpb44 <juanjpb@proton.me>
2026-09-14 22:50:13 -0500

    Cycle to first tab at the 9-tab limit (optional, default on)

--- main.lua
+++ main.lua
@@ -1,11 +1,21 @@
 --- @sync entry
 
-return {
-	entry = function()
-		if cx.tabs.idx >= #cx.tabs then
-			ya.emit("tab_create", { current = true })
-		else
-			ya.emit("tab_switch", { 1, relative = true })
-		end
-	end,
-}
+local M = { cycle = true }
+
+function M:setup(opts)
+	if opts and opts.cycle ~= nil then
+		M.cycle = opts.cycle
+	end
+end
+
+M.entry = function()
+	if cx.tabs.idx < #cx.tabs then
+		ya.emit("tab_switch", { 1, relative = true })
+	elseif M.cycle and #cx.tabs >= 9 then
+		ya.emit("tab_switch", { 0 })
+	else
+		ya.emit("tab_create", { current = true })
+	end
+end
+
+return M
